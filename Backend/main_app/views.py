500b3424bf094c57c54029b5a80ad83d39f3adaa
Steven V <sgvillacis@gmail.com>
2024-07-18 16:27:54 -0400

    Completed views and urls

--- Backend/main_app/views.py
+++ Backend/main_app/views.py
@@ -1,9 +1,50 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from .models import (
+    User,
+    Hackathon,
+    Badge,
+    Team,
+    Skill,
+)
+from .serializers import (
+    UserSerializer,
+    CategorySerializer,
+    HackathonSerializer,
+    BadgeSerializer,
+    TeamSerializer,
+    SkillSerializer,
+    )
+
 
 # Create your views here.
-class Home(APIView):
-    def get(self, request):
-        content = {'message': 'Welcome to DP!'}
-        return Response(content)
+
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserRUD(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class HackathonList(generics.ListAPIView):
+    queryset = Hackathon.objects.all()
+    serializer_class = HackathonSerializer
+
+class HackathonDetail(generics.RetrieveAPIView):
+    queryset = Hackathon.objects.all()
+    serializer_class = HackathonSerializer
+
+class TeamListCreate(generics.ListCreateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class TeamRUD(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class SkillList(generics.ListAPIView):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
