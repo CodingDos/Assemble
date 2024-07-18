2ebcd2623e2ad125bf83be2d9853395b0f436857
John Lopez <john.lopez3695@gmail.com>
2024-07-18 16:27:04 -0400

    initialized views and urls for CRUD

--- Backend/main_app/urls.py
+++ Backend/main_app/urls.py
@@ -1,8 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import Home
+from .views import UserListCreate, UserRUD, HackathonList, HackathonDetail, TeamListCreate, TeamRUD, SkillList
 
 urlpatterns = [
-    path('', Home.as_view(), name='home'),
+    path('users/', UserListCreate.as_view(), name='UserListCreate'),
+    path('users/<int:pk>/', UserRUD.as_view(), name='UserRUD'),
+    
+    path('hackathons/', HackathonList.as_view(), name='HackathonList'),
+    path('hackathons/<int:pk>/', HackathonDetail.as_view(), name='HackathonDetail'),
+   
+    path('teams/', TeamListCreate.as_view(), name='TeamListCreate'),
+    path('teams/<int:pk>', TeamRUD.as_view(), name='TeamRUD'),
+   
+    path('skills/', SkillList.as_view(), name='SkillList')
+    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Backend/main_app/views.py
+++ Backend/main_app/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import User, Hackathon, Team, Skill
+from .serializers import UserSerializer, HackathonSerializer, TeamSerializer, SkillSerializer
 
 # Create your views here.
-class Home(APIView):
-    def get(self, request):
-        content = {'message': 'Welcome to DP!'}
-        return Response(content)
+
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserRUD(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class HackathonList(generics.ListView):
+    queryset = Hackathon.objects.all()
+    serializer_class = HackathonSerializer
+
+class HackathonDetail(generics.RetrieveUpdateAPIView):
+    queryset = Hackathon.objects.all()
+    serializer_class = HackathonSerializer
+
+class TeamListCreate(generics.ListCreateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class TeamRUD(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class SkillList(generics.ListView):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
