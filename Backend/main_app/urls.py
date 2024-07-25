e1ffde43bb8bb6d90a1b929b7a9572825103459d
Steven V <sgvillacis@gmail.com>
2024-07-25 16:38:19 -0400

    added auth for user login / logout

--- Backend/main_app/urls.py
+++ Backend/main_app/urls.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from .auth import userLogin, userLogout
 from .views import UserListCreate, UserRUD, HackathonList, HackathonDetail, TeamListCreate, TeamRUD, SkillList
 
 urlpatterns = [
     path('users/', UserListCreate.as_view(), name='UserListCreate'),
+    path('login/', userLogin, name='login'),
+    path('logout/', userLogout, name='logout'),
     path('users/<int:pk>/', UserRUD.as_view(), name='UserRUD'),
     
     path('hackathons/', HackathonList.as_view(), name='HackathonList'),
