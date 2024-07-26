5877b696aba17829178ff49ea88d29c16566044d
John Lopez <john.lopez3695@gmail.com>
2024-07-26 14:19:35 -0400

    added route for hackathon detail page

--- Frontend/assemble/src/App.jsx
+++ Frontend/assemble/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Landing from './Pages/Landing/Landing.jsx'
 import Auth from './Pages/Auth/Auth.jsx'
 import "./App.css";
+import HackathonDetail from "./Pages/Hackathon/HackathonDetail.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Landing />}/>
         <Route path='/auth' element={<Auth />}/>
+        <Route path='/hackathon/:id' element={<HackathonDetail />} />
       </Routes>
     </>
   )
